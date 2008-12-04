3ae8e9112a371125b09a6ee2ee433fdfecc39078
David Harte <david@statsresearch.co.nz>
2008-12-04 00:00:00 +0000

    version 3.1-2

---
+++ data/Phuket.R
@@ -0,0 +1,7 @@
+Phuket <- scan("Phuket.txt", what=list(
+                 latitude=0, longitude=0, depth=0, magnitude=0,
+                 time=0, hour=0, minute=0, second=0, day=0, 
+                 month=0, year=0), skip=0, quiet=TRUE)
+
+Phuket <- as.data.frame(Phuket)
+
